147b422908ce7fec0665d01a20f173fa6da5318b
mulbinbich <mulbinbich@gmail.com>
2022-04-19 15:56:07 +0900

    mykey 수정

--- routes/user.js
+++ routes/user.js
@@ -22,8 +22,8 @@ router.post("/login", async (req, res) => {
     //     res.status(400).send({errorMessage: '이메일 또는 비밀번호를 확인해주세요'});
     //     return;
     // } else {
-        const token = jwt.sign({ email : user.email, nickName : user.nickName, userprofile : user.userprofile },process.env.JWT_SECRET);
-        res.json({token, userinfo})
+        const token = jwt.sign({ email : user.email, nickName : user.nickName, userprofile : user.userprofile }, mykey);
+        res.json({token})
     // }
 });
 // 회원가입
